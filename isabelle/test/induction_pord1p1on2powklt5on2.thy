adba845e101cd4fca9d646cd67caddf6ff9ef81a
Qiyuan Xu <xqyww123@gmail.com>
2026-05-16 13:41:53 +0800

    correct

--- isabelle/test/induction_pord1p1on2powklt5on2.thy
+++ isabelle/test/induction_pord1p1on2powklt5on2.thy
@@ -9,7 +9,7 @@ begin
 theorem induction_pord1p1on2powklt5on2:
   fixes n :: nat
   assumes "0 < n" 
-  shows "(\<Prod> k \<in>{1..<n+1}. (1 + 1 / 2^k)) < 5 / 2"
+  shows "(\<Prod> k \<in>{1..<n+1}. (1 + 1 / 2^k)) < (5::rat) / 2"
   sorry
 
 end
